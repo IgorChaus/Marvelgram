db130eb1760bb4c785836a6721e7bc9236727fcc
Igor Chaus <igor_chaus@mail.ru>
2023-05-06 08:45:03 +0200

    Moved launch MainFragment to the layout

--- app/src/main/java/com/example/marvelvm/MainActivity.kt
+++ app/src/main/java/com/example/marvelvm/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.example.marvelvm.view.MainFragment
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
@@ -13,14 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.getInstance())
-                .commitNow()
-        }
     }
 
     //Обрабатываем нажатие стрелки назад
